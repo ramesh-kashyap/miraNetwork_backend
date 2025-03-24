0877baf3d1a1c3aa3e40ed8198f2114062b1dbf0
Sachink03 <prajapatsachin835@gmail.com>
2025-03-24 15:21:39 +0530

    sach

--- src/controllers/TelegramController.js
+++ src/controllers/TelegramController.js
@@ -14,41 +14,20 @@ let timeNow = Date.now();
 
 const getUserByTelegramId = async (req, res) => {
     try {
-        const { telegram_id } = req.body;
-
+        const { telegram_id } = req.user;
+        console.log(req.user?.telegram_id);
+        const tid = req.user?.telegram_id
         if (!telegram_id) {
-            return res.status(400).json({
+            return res.status(200).json({
                 message: "Telegram ID is required",
                 status: false,
                 timeStamp: timeNow,
             });
         }
-           
-         console.log( telegram_id);
-
-
-        const query = `
-            SELECT 
-                tu.telegram_id, tu.tusername, tu.tname, tu.tlastname,
-                u.id AS user_id, u.email, u.name, u.username
-            FROM telegram_users tu
-            LEFT JOIN users u ON tu.id = u.telegram_id
-            WHERE tu.telegram_id = :telegram_id;
-        `;
-        // Use Sequelize `query()` instead of `mysql.execute()`
-        const results = await sequelize.query(query, {
-            replacements: { telegram_id },  // Use replacements for security
-            type: QueryTypes.SELECT         // Ensures correct result format
-        });
-
-        if (results.length === 0) {
-            return res.status(404).json({
-                message: "User not found",
-                status: false,
-                timeStamp: timeNow,
-            });
+        const user = await TelegramUser.findOne({where:{telegram_id: tid}}) 
+        if(!user){
+            return res.status(200).json(message, "user not found");
         }
-
         return res.status(200).json({
             user: results[0],
             status: true,
